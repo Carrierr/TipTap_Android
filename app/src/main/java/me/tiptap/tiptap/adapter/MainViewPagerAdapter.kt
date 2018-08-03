d5a62a9e90395af4ece5abcb1c633679caa78691
Jasonarea <hyeonsuns@naver.com>
2018-08-03 12:53:48 +0900

    ScratchView Adapter Modification

--- app/src/main/java/me/tiptap/tiptap/adapter/MainViewPagerAdapter.kt
+++ app/src/main/java/me/tiptap/tiptap/adapter/MainViewPagerAdapter.kt
@@ -3,6 +3,7 @@ package me.tiptap.tiptap.adapter
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
+import me.tiptap.tiptap.DiariesFragment
 import me.tiptap.tiptap.MainFragment
 import me.tiptap.tiptap.ScratchFragment
 
@@ -10,6 +11,7 @@ class MainViewPagerAdapter(fm: FragmentManager, val pageCount: Int) : FragmentSt
 
 
     override fun getItem(position: Int): Fragment = when (position) {
+        0 -> DiariesFragment()
         1 -> MainFragment()
         2 -> ScratchFragment()
         else -> MainFragment()
