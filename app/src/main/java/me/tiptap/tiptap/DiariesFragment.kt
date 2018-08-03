e9d012d1fd86d7a629a7c950c94985f23b774568
Jiyunn <jygr0309@gmail.com>
2018-08-03 10:45:53 +0900

    #8-DiariesFragment 추가

---
+++ app/src/main/java/me/tiptap/tiptap/DiariesFragment.kt
@@ -0,0 +1,42 @@
+package me.tiptap.tiptap
+
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import me.tiptap.tiptap.databinding.FragmentDiariesBinding
+
+class DiariesFragment : Fragment() {
+
+    private lateinit var binding : FragmentDiariesBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_diaries, container, false)
+
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        binding.recyclerDiaries.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@DiariesFragment.context)
+
+            adapter = DiaryAdapter().apply {
+                clickSubject.subscribe {
+                    //go to detail page.
+                }
+            }
+        }
+    }
+
+
+}
--- app/src/main/java/me/tiptap/tiptap/adapter/MainViewPagerAdapter.kt
+++ app/src/main/java/me/tiptap/tiptap/adapter/MainViewPagerAdapter.kt
@@ -3,13 +3,14 @@ package me.tiptap.tiptap.adapter
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
+import me.tiptap.tiptap.DiariesFragment
 import me.tiptap.tiptap.MainFragment
 
 class MainViewPagerAdapter(fm: FragmentManager, val pageCount: Int) : FragmentStatePagerAdapter(fm) {
 
 
     override fun getItem(position: Int): Fragment = when (position) {
-//        0 ->
+        0 -> DiariesFragment()
         1 -> MainFragment()
         else -> MainFragment()
     }
