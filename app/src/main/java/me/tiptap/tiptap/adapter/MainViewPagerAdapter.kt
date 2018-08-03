a38be7a3a048ba9a43fe291d76dc035f0c9fdcf7
Jasonarea <hyeonsuns@naver.com>
2018-08-03 12:57:41 +0900

    scratch package 생성

--- app/src/main/java/me/tiptap/tiptap/adapter/MainViewPagerAdapter.kt
+++ app/src/main/java/me/tiptap/tiptap/adapter/MainViewPagerAdapter.kt
@@ -4,8 +4,8 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
 import me.tiptap.tiptap.MainFragment
-import me.tiptap.tiptap.ScratchFragment
 import me.tiptap.tiptap.diaries.DiariesFragment
+import me.tiptap.tiptap.scratch.ScratchFragment
 
 class MainViewPagerAdapter(fm: FragmentManager, val pageCount: Int) : FragmentStatePagerAdapter(fm) {
 
