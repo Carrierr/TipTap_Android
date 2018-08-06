5689125a9295945924fbe2ec102d48971f65048b
Jiyunn <jygr0309@gmail.com>
2018-08-06 16:56:56 +0900

    #16-DiaryDetailActivity 추가

---
+++ app/src/main/java/me/tiptap/tiptap/diarydetail/DiaryDetailActivity.kt
@@ -0,0 +1,20 @@
+package me.tiptap.tiptap.diarydetail
+
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import me.tiptap.tiptap.R
+import me.tiptap.tiptap.databinding.ActivityDiaryDetailBinding
+
+class DiaryDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityDiaryDetailBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_diary_detail)
+        
+    }
+
+}
