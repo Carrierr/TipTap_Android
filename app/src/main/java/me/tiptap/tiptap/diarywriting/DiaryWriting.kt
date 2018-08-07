6a66951fbcd46d9fcec4920d94d0c1fe2fd0cc79
Jasonarea <hyeonsuns@naver.com>
2018-08-07 12:19:49 +0900

    #19-Add textview event

--- app/src/main/java/me/tiptap/tiptap/diarywriting/DiaryWriting.kt
+++ app/src/main/java/me/tiptap/tiptap/diarywriting/DiaryWriting.kt
@@ -1,18 +1,26 @@
 package me.tiptap.tiptap.diarywriting
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import me.tiptap.tiptap.R
-import me.tiptap.tiptap.databinding.ActivityMainBinding
+import me.tiptap.tiptap.databinding.ActivityDiaryWritingBinding
 
 class DiaryWriting : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding : ActivityDiaryWritingBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_diary_writing)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_diary_writing)
+
+        binding.complete.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(view: View):Unit{
+                finish()
+            }
+        })
 
     }
 }
