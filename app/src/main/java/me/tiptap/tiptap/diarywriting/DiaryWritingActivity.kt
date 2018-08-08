60d2114e21ae22834b3c9a13c0b471ad1e5cd22f
Jasonarea <hyeonsuns@naver.com>
2018-08-09 01:46:07 +0900

    #23-Add Location Setting Popup window

--- app/src/main/java/me/tiptap/tiptap/diarywriting/DiaryWritingActivity.kt
+++ app/src/main/java/me/tiptap/tiptap/diarywriting/DiaryWritingActivity.kt
@@ -1,11 +1,16 @@
 package me.tiptap.tiptap.diarywriting
 
+import android.content.DialogInterface
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.EditText
 import me.tiptap.tiptap.R
 import me.tiptap.tiptap.databinding.ActivityDiaryWritingBinding
+
+
 
 class DiaryWritingActivity : AppCompatActivity() {
 
@@ -27,7 +32,20 @@ class DiaryWritingActivity : AppCompatActivity() {
 
         binding.location.setOnClickListener(object: View.OnClickListener{
             override fun onClick(view: View):Unit{
+                val alert = AlertDialog.Builder(this@DiaryWritingActivity)
+                alert.setTitle("위치입력")
 
+                val input = EditText(this@DiaryWritingActivity)
+                alert.setView(input)
+
+                alert.setNegativeButton("취소", null)
+                alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialog, whichButton ->
+                    val place = input.text.toString()
+                    // Do something with value!
+                    binding.location.setText(place)
+                })
+
+                alert.show()
             }
         })
     }
