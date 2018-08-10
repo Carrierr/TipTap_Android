ecf171041001e02a1d815c80a2c83841d336dbc8
Jasonarea <hyeonsuns@naver.com>
2018-08-11 00:56:03 +0900

    #29-implement uploading picture & toolbar

--- app/src/main/java/me/tiptap/tiptap/diarywriting/DiaryWritingActivity.kt
+++ app/src/main/java/me/tiptap/tiptap/diarywriting/DiaryWritingActivity.kt
@@ -1,23 +1,25 @@
 package me.tiptap.tiptap.diarywriting
 
+import android.app.Activity
 import android.content.DialogInterface
+import android.content.Intent
 import android.databinding.DataBindingUtil
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import me.tiptap.tiptap.R
 import me.tiptap.tiptap.databinding.ActivityDiaryWritingBinding
+import java.io.IOException
 
 
 class DiaryWritingActivity : AppCompatActivity()  {
 
     private lateinit var binding : ActivityDiaryWritingBinding
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,6 +51,29 @@ class DiaryWritingActivity : AppCompatActivity()  {
                 alert.show()
             }
         })
+        binding.gallery.setOnClickListener {
+            val intent = Intent()
+            intent.setType("image/*")
+            intent.setAction(Intent.ACTION_GET_CONTENT)
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1)
+        }
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 1) {
+            if(resultCode == Activity.RESULT_OK){
+                if(data !=null){
+                    try{
+                        val bitmap:Bitmap = MediaStore.Images.Media.getBitmap(contentResolver, data.data )
+                        binding.myPicture.setImageBitmap(bitmap)
+                    }catch (e:IOException){
+                        e.printStackTrace()
+                    }
+                }else if(resultCode == Activity.RESULT_CANCELED){
+                    Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
