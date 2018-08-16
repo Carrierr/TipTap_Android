0ad95409d6b9e8602b0e53d1a8caf38ef2ca3a13
Jiyunn <jygr0309@gmail.com>
2018-08-17 00:23:20 +0900

    #47-로그인 화면 이동 로직 추가

--- app/src/main/java/me/tiptap/tiptap/splash/SplashActivity.kt
+++ app/src/main/java/me/tiptap/tiptap/splash/SplashActivity.kt
@@ -2,17 +2,63 @@ package me.tiptap.tiptap.splash
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import com.kakao.auth.ISessionCallback
+import com.kakao.auth.Session
+import com.kakao.util.exception.KakaoException
 import me.tiptap.tiptap.R
 import me.tiptap.tiptap.main.MainActivity
+import me.tiptap.tiptap.util.redirectLoginActivity
 
 class SplashActivity : AppCompatActivity() {
+
+    private val delayMill = 1200L //delay milliseconds
+
+    private val callback = object : ISessionCallback {
+        override fun onSessionOpened() { //If session is opened, go to main activity.
+            goToMainActivity()
+        }
+
+        override fun onSessionOpenFailed(exception: KakaoException?) { //go to login activity.
+            redirectLoginActivity()
+            finish()
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        startActivity(Intent(this, MainActivity::class.java))
+
+        Session.getCurrentSession().addCallback(callback)
+
+        scheduleSplashScreen()
+    }
+
+    private fun scheduleSplashScreen() {
+        Handler().postDelayed({
+            if (!Session.getCurrentSession().checkAndImplicitOpen()) { //Check session
+                redirectLoginActivity()
+                finish()
+            }
+        }, delayMill)
+
+    }
+
+
+    private fun goToMainActivity() {
+        startActivity(Intent(this@SplashActivity, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+        })
         finish()
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Session.getCurrentSession().removeCallback(callback)
     }
 }
