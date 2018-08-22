684b5b15ba188a6774fc479388cdeff09c4d748e
Jiyunn <jygr0309@gmail.com>
2018-08-22 16:28:49 +0900

    #85-DiaryActivity 수정

--- app/src/main/java/me/tiptap/tiptap/diarydetail/DiaryDetailActivity.kt
+++ app/src/main/java/me/tiptap/tiptap/diarydetail/DiaryDetailActivity.kt
@@ -4,45 +4,42 @@ import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import me.tiptap.tiptap.R
 import me.tiptap.tiptap.databinding.ActivityDiaryDetailBinding
 import me.tiptap.tiptap.util.setupActionBar
+import java.util.*
 
 class DiaryDetailActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityDiaryDetailBinding
+    private lateinit var binding: ActivityDiaryDetailBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_diary_detail)
 
-        setupActionBar(R.id.toolbar) {
+        setupActionBar(R.id.toolbar_detail) {
+            setDisplayShowTitleEnabled(false)
+            setHomeAsUpIndicator(R.drawable.ic_back_white)
             setDisplayHomeAsUpEnabled(true)
         }
-    }
 
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_detail, menu)
-
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.menu_detail_delete) {
-            AlertDialog.Builder(this).apply {
-                setMessage(R.string.ask_delete)
-                setNegativeButton(R.string.cancel, null)
-                setPositiveButton(R.string.ok) {_, _ ->
-                    finish()
-                }
-
-                show()
-            }
+        binding.apply {
+            activity = this@DiaryDetailActivity
+            date = Date()
         }
-        return false
+    }
+
+    fun onDeleteButtonClick() {
+        AlertDialog.Builder(this).apply {
+            setMessage(R.string.ask_delete)
+            setNegativeButton(R.string.cancel, null)
+            setPositiveButton(R.string.ok) {_, _ ->
+                //do something
+                finish()
+            }
+
+            show()
+        }
     }
 }
