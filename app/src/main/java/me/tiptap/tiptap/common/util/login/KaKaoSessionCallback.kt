73b70122ef06c3b042fba90870736e67e8d7d536
Jiyunn <jygr0309@gmail.com>
2018-09-18 12:24:34 +0900

    #114-SessionCallback 클래스 수정 및 필요한 string 정의

--- app/src/main/java/me/tiptap/tiptap/common/util/login/KaKaoSessionCallback.kt
+++ app/src/main/java/me/tiptap/tiptap/common/util/login/KaKaoSessionCallback.kt
@@ -8,10 +8,19 @@ import com.kakao.usermgmt.callback.MeV2ResponseCallback
 import com.kakao.usermgmt.response.MeV2Response
 import com.kakao.util.exception.KakaoException
 import com.kakao.util.helper.log.Logger
+import me.tiptap.tiptap.R
+import me.tiptap.tiptap.TipTapApplication
 import me.tiptap.tiptap.data.User
 
 class KaKaoSessionCallback(val navigator: LoginNavigator) : ISessionCallback {
 
+    private var type: String = ""
+
+    init {
+        TipTapApplication.getTipTapApplicationContext()?.let {
+            type = it.getString(R.string.type_kakao)
+        }
+    }
 
     override fun onSessionOpenFailed(exception: KakaoException?) {
         if (exception != null) Logger.e(exception)
@@ -28,7 +37,7 @@ class KaKaoSessionCallback(val navigator: LoginNavigator) : ISessionCallback {
                 //result 가 null 이 아니면 유저의 정보 담아서 보냄.
                 result?.let {
                     navigator.getAccessToken(
-                            User("kakao",
+                            User(type,
                                     it.id.toString(),
                                     it.nickname))
                 }
