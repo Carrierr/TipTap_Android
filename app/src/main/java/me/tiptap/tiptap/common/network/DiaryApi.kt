139a26eea5044628123d6ac86553cd4129cc93a2
Jiyunn <jygr0309@gmail.com>
2018-09-20 00:16:51 +0900

    #117-DiaryApi 수정

--- app/src/main/java/me/tiptap/tiptap/common/network/DiaryApi.kt
+++ app/src/main/java/me/tiptap/tiptap/common/network/DiaryApi.kt
@@ -5,6 +5,7 @@ import io.reactivex.Observable
 import me.tiptap.tiptap.data.Diary
 import me.tiptap.tiptap.data.User
 import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.http.*
 
 interface DiaryApi {
@@ -26,11 +27,12 @@ interface DiaryApi {
     @POST("diary/write")
     fun writeDiary(
             @Header("tiptap-token") token: String,
-            @Part("content") content: String,
-            @Part("location") location: String,
-            @Part("latitude") latitude: String,
-            @Part("longitude") longitude: String,
-            @Part diaryFile: MultipartBody.Part)
+            @Part("content") content: RequestBody,
+            @Part("location") location: RequestBody,
+            @Part("latitude") latitude: RequestBody,
+            @Part("longitude") longitude: RequestBody,
+            @Part diaryFile: MultipartBody.Part?)
+            : Observable<JsonObject>
 
 
     //delete Diary
