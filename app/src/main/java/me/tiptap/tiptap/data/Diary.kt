db8bdee77712e6a4f769463bce1a5d22ccfc91a7
Jiyunn <jygr0309@gmail.com>
2018-09-20 22:06:07 +0900

    #121-Diary 클래스 수정

--- app/src/main/java/me/tiptap/tiptap/data/Diary.kt
+++ app/src/main/java/me/tiptap/tiptap/data/Diary.kt
@@ -7,17 +7,22 @@ import java.util.*
 class Diary() {
     var id: Int = 0
     var date: Date? = null
+    var photo: Uri? = null
     //real val
     var content: String = ""
     var location: String = ""
-    var photo: Uri? = null
-    var isSelected: Boolean = false
+    var imageUrl: String? = null
     var latitude: String = ""
     var longitude: String = ""
+    var shared: Boolean = true
+    var createdAt: String = ""
+    var updatedAt: String = ""
     var diaryFile: MultipartBody.Part? = null
 
+    var isNewMonth : Boolean = false
+    var isSelected: Boolean = false
 
-    constructor(id: Int, date: Date, content: String, location : String, photo: Uri) : this() {
+    constructor(id: Int, date: Date, content: String, location: String, photo: Uri) : this() {
         this.id = id
         this.date = date
         this.content = content
