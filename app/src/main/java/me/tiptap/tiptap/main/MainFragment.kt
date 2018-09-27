47043958b6e4c743c8edb1ba2048cd0b50768d11
Jiyunn <jygr0309@gmail.com>
2018-09-27 14:27:21 +0900

    #148-오늘의 다이어리 가져오는 API 적용

--- app/src/main/java/me/tiptap/tiptap/main/MainFragment.kt
+++ app/src/main/java/me/tiptap/tiptap/main/MainFragment.kt
@@ -2,15 +2,25 @@ package me.tiptap.tiptap.main
 
 import android.content.Intent
 import android.databinding.DataBindingUtil
-import android.databinding.ObservableField
+import android.databinding.ObservableInt
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableObserver
+import io.reactivex.schedulers.Schedulers
 import me.tiptap.tiptap.R
+import me.tiptap.tiptap.TipTapApplication
+import me.tiptap.tiptap.common.network.DiaryApi
+import me.tiptap.tiptap.common.network.ServerGenerator
 import me.tiptap.tiptap.common.rx.RxBus
+import me.tiptap.tiptap.data.Diary
+import me.tiptap.tiptap.data.TodayResponse
 import me.tiptap.tiptap.databinding.FragmentMainBinding
 import me.tiptap.tiptap.diarywriting.DiaryWritingActivity
 import me.tiptap.tiptap.setting.SettingActivity
@@ -20,21 +30,18 @@ class MainFragment : Fragment() {
 
     private lateinit var binding: FragmentMainBinding
 
+    private val service = ServerGenerator.createService(DiaryApi::class.java)
     private val rxBus = RxBus.getInstance()
-    val postSize: ObservableField<Int> = ObservableField(1) //post size
+    private val disposables = CompositeDisposable()
 
+    var todayDiaries: MutableList<Diary> = mutableListOf()
+    val postSize = ObservableInt(todayDiaries.size) //postSize
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
         binding.apply {
             fragment = this@MainFragment
             date = Date()
-
-            btnMainAdd.setOnClickListener {
-                Intent(context, DiaryWritingActivity::class.java).apply {
-                    startActivity(this)
-                }
-            }
         }
 
         return binding.root
@@ -48,15 +55,66 @@ class MainFragment : Fragment() {
     }
 
 
+    override fun onResume() {
+        super.onResume()
+
+        getTodayDiaries()
+    }
+
+
+    /**
+     * Get today diaries
+     */
+    private fun getTodayDiaries() {
+        disposables.add(
+                service.getTodayDiaries(TipTapApplication.getAccessToken())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeOn(Schedulers.io())
+                        .subscribeWith(object : DisposableObserver<TodayResponse>() {
+                            override fun onNext(t: TodayResponse) {
+                                todayDiaries = t.data.diaries
+                                postSize.set(todayDiaries.size)
+
+                                applyStamps(t.data.stamp)
+                            }
+
+                            override fun onComplete() {
+
+                            }
+
+                            override fun onError(e: Throwable) {
+                                e.printStackTrace()
+                            }
+                        }))
+
+    }
+
+    /**
+     * apply stamps
+     */
+    private fun applyStamps(stamps: MutableList<String>) {
+        for (i in 1 until stamps.size - 1) {
+            val stampId = resources.getIdentifier(stamps[i], "drawable", activity?.packageName)
+            val viewId = resources.getIdentifier("img_post_${i + 1}", "id", activity?.packageName)
+
+            val view = activity?.findViewById(viewId) as ImageView
+
+            view.setImageResource(stampId)
+        }
+    }
+
+
     private fun initToolbar() {
         (activity as AppCompatActivity).apply {
             setSupportActionBar(binding.toolbar?.toolbarMain)
             supportActionBar?.setDisplayShowTitleEnabled(false)
         }
+    }
 
-        binding.toolbar?.apply {
-            textToolbarMainTitle.text = getString(R.string.today)
-            textToolbarMainSub.text = getString(R.string.post_count, postSize.get().toString())
+
+    fun onAddButtonClick() {
+        Intent(context, DiaryWritingActivity::class.java).apply {
+            startActivity(this)
         }
     }
 
@@ -64,4 +122,17 @@ class MainFragment : Fragment() {
         startActivity(Intent(this@MainFragment.activity, SettingActivity::class.java))
     }
 
+
+    override fun onPause() {
+        super.onPause()
+
+        disposables.clear()
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        disposables.dispose()
+    }
 }
