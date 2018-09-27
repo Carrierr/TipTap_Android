27835312ecd3b7e95f3d45eb8b5c373a71391fcf
Jasonarea <hyeonsuns@naver.com>
2018-09-27 22:07:07 +0900

    #153-Custom Login Button 적용

---
+++ app/src/main/java/me/tiptap/tiptap/login/KakaoLoginControl.kt
@@ -0,0 +1,30 @@
+package me.tiptap.tiptap.login
+
+import android.app.Activity
+import android.content.Context
+import com.kakao.auth.AuthType
+import com.kakao.auth.Session
+import com.kakao.usermgmt.LoginButton
+
+
+class KakaoLoginControl(context: Context) : LoginButton(context) {
+
+    fun call() {
+        try {
+            val methodGetAuthTypes = LoginButton::class.java.getDeclaredMethod("getAuthTypes")
+            methodGetAuthTypes.isAccessible = true
+            val authTypes = methodGetAuthTypes.invoke(this) as List<AuthType>
+            if (authTypes.size == 1) {
+                Session.getCurrentSession().open(authTypes[0], context as Activity)
+            } else {
+                val methodOnClickLoginButton = LoginButton::class.java.getDeclaredMethod("onClickLoginButton", LoginActivity::class.java)
+                methodOnClickLoginButton.isAccessible = true
+                methodOnClickLoginButton.invoke(this, authTypes)
+            }
+        } catch (e: Exception) {
+            Session.getCurrentSession().open(AuthType.KAKAO_ACCOUNT, context as Activity)
+        }
+
+    }
+
+}
