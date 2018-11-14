f873ef83e8cf50b8c34a3ad5fe9a088d76d8893d
jiyunn <jygr0309@gmail.com>
2018-11-15 01:29:40 +0900

    #234-Modify OnBoarding file names.

--- app/src/main/java/me/tiptap/tiptap/onboarding/OnBoardingFragment3.kt
+++ app/src/main/java/me/tiptap/tiptap/onboarding/OnBoardingFragment3.kt
@@ -1,21 +1,21 @@
 package me.tiptap.tiptap.onboarding
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import me.tiptap.tiptap.R
+import me.tiptap.tiptap.databinding.FragmentOnboard3Binding
 
 
 class OnBoardingFragment3 : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(
-                R.layout.onboarding_screen3,
-                container,
-                false
-        )
+    private  lateinit var  binding : FragmentOnboard3Binding
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_onboard3, container, false)
+        return binding.root
     }
 }
