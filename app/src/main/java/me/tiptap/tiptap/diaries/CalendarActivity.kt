09b1e138739a41f75e9fb6c79e212f0020064dd9
Jiyunn <jygr0309@gmail.com>
2018-11-20 16:24:40 +0900

    #253-Apply user can't select an invalid date range.

--- app/src/main/java/me/tiptap/tiptap/diaries/CalendarActivity.kt
+++ app/src/main/java/me/tiptap/tiptap/diaries/CalendarActivity.kt
@@ -2,7 +2,9 @@ package me.tiptap.tiptap.diaries
 
 import android.app.DatePickerDialog
 import android.databinding.DataBindingUtil
+import android.databinding.ObservableBoolean
 import android.os.Bundle
+import android.support.annotation.IdRes
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.View
@@ -10,6 +12,7 @@ import android.widget.TextView
 import me.tiptap.tiptap.R
 import me.tiptap.tiptap.common.rx.RxBus
 import me.tiptap.tiptap.databinding.ActivityCalendarBinding
+import java.text.SimpleDateFormat
 import java.util.*
 
 class CalendarActivity : AppCompatActivity() {
@@ -46,22 +49,47 @@ class CalendarActivity : AppCompatActivity() {
 
 
     fun onCalendarOpenClick(view: View) {
-        val calendar = Calendar.getInstance()
+        Calendar.getInstance().apply {
+            val year = get(Calendar.YEAR)
+            val month = get(Calendar.MONTH)
+            val day = get(Calendar.DAY_OF_MONTH)
 
-        val year = calendar.get(Calendar.YEAR)
-        val month = calendar.get(Calendar.MONTH)
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
+            val datePickerDialog = DatePickerDialog(this@CalendarActivity, R.style.BaseDatePickerTheme,
+                    DatePickerDialog.OnDateSetListener { _, pickYear, pickMonth, pickDay ->
+                        if (view is TextView) {
+                            val dateText = getString(R.string.date_format, pickYear, pickMonth + 1, pickDay)
 
-        val datePickerDialog = DatePickerDialog(this, R.style.BaseDatePickerTheme,
-                DatePickerDialog.OnDateSetListener { _, pickYear, pickMonth, pickDay ->
-                    if (view is TextView) {
-                        view.setTextColor(ContextCompat.getColor(this@CalendarActivity, R.color.colorSeaFoamBlue))
-                        view.text = getString(R.string.date_format, pickYear, pickMonth + 1, pickDay)
-                    }
-                }, year, month, day)
+                            if (checkDateIsValid(dateText, view.id)) { //if date is valid.
+                                view.setTextColor(ContextCompat.getColor(this@CalendarActivity, R.color.colorSeaFoamBlue))
+                                view.text = dateText
+                            }
+                        }
+                    }, year, month, day)
 
-        if (!datePickerDialog.isShowing) {
-            datePickerDialog.show()
+            if (!datePickerDialog.isShowing) {
+                datePickerDialog.show()
+            }
+        }
+    }
+
+    /**
+     * Check  whether the Date is valid .
+     * 1. startDate & endDate can not be over today.
+     * 2. startDate must be less than endDate.
+     * 3. endDate must be greater than startDate.
+     */
+    private fun checkDateIsValid(dateText: String, @IdRes viewId: Int): Boolean {
+        val dateFormat = SimpleDateFormat("yyyy.MM.dd", Locale.KOREAN)
+        val pickDate = dateFormat.parse(dateText)
+
+        val startDate = dateFormat.parse(binding.textCalStart.text.toString())
+        val endDate = dateFormat.parse(binding.textCalEnd.text.toString())
+
+        return when (viewId) {
+            R.id.text_cal_start -> (Date().compareTo(pickDate)) > 0 && (pickDate <= endDate)
+            R.id.text_cal_end -> (Date().compareTo(pickDate)) > 0 && (pickDate >= startDate)
+
+            else -> false
         }
     }
 }
