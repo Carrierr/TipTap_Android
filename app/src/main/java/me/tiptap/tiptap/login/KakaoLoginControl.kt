5d6efa33fab6d9054dc64c6285af71a2cc1b434a
Jiyunn <jygr0309@gmail.com>
2018-12-04 22:36:13 +0900

    #273-Token storage condition modified.

--- app/src/main/java/me/tiptap/tiptap/login/KakaoLoginControl.kt
+++ app/src/main/java/me/tiptap/tiptap/login/KakaoLoginControl.kt
@@ -14,17 +14,18 @@ class KakaoLoginControl(context: Context) : LoginButton(context) {
             val methodGetAuthTypes = LoginButton::class.java.getDeclaredMethod("getAuthTypes")
             methodGetAuthTypes.isAccessible = true
             val authTypes = methodGetAuthTypes.invoke(this) as List<AuthType>
-            if (authTypes.size == 1) {
-                Session.getCurrentSession().open(authTypes[0], context as Activity)
-            } else {
-                val methodOnClickLoginButton = LoginButton::class.java.getDeclaredMethod("onClickLoginButton", LoginActivity::class.java)
-                methodOnClickLoginButton.isAccessible = true
-                methodOnClickLoginButton.invoke(this, authTypes)
-            }
+
+                if (authTypes.size == 1) {
+                    Session.getCurrentSession().open(authTypes[0], context as Activity)
+                } else {
+                    LoginButton::class.java.getDeclaredMethod("onClickLoginButton", LoginActivity::class.java).apply {
+                        isAccessible = true
+                        invoke(this, authTypes)
+                    }
+                }
         } catch (e: Exception) {
             Session.getCurrentSession().open(AuthType.KAKAO_ACCOUNT, context as Activity)
         }
-
     }
 
 }
